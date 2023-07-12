8dd6150476e30e6ffe86c12f2e400c6d69ce49e6
solfonte <msfontenla@fi.uba.ar>
2023-07-12 20:01:52 -0300

    feat: receive rules file path

--- fifa-review.go
+++ fifa-review.go
@@ -11,13 +11,15 @@ import (
 func main() {
 	
 	var match_file_paths utils.FlagsArray
-	flag.Var(&match_file_paths, "match", "matches files to iterate")
+	flag.Var(&match_file_paths, "match", "matches files")
+	rules_file_path := flag.String("rules", "", "rules file")
     flag.Parse()
 
 	if len(match_file_paths) == 0 {
 		fmt.Println("Please provide at least one match file path")
-
 	}
 
-	fmt.Println(match_file_paths)
+	if len(*rules_file_path) != 0 {
+		//do something
+	}
 }
